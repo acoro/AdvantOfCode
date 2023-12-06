da5204eab1169a2ff93daa5031f943567765b776
Acoro <corolleur.anthony@gmail.com>
2023-12-07 00:12:09 +0100

    Implémentation toujours trop longue !!!

--- Ex5/Ex5_2.py
+++ Ex5/Ex5_2.py
@@ -10,6 +10,8 @@ Lecture_Seeds = True
 Fichier_Ligne = ""
 Liste_Seeds2Location_Interval=[]
 Liste_Seeds2Location=[]
+Liste_Seeds_Status=[]
+Break =0
 
 while Lecture_Boucle:
     Fichier_Ligne = Fichier_Input.readline()
@@ -25,29 +27,77 @@ while Lecture_Boucle:
                 Destination_Interval=0
              else:
                 Destination_Interval=int(Liste_Seeds2Location_Interval[i])+Source_Interval-1
-             for i in range(Source_Interval,Destination_Interval):
-                Liste_Seeds2Location.append(i)
-                #print("Trop long !!!!!")                
-          Liste_Seeds_Status=[0]*(len(Liste_Seeds2Location))
+                Liste_Seeds2Location.append([Source_Interval,Destination_Interval])
+                Liste_Seeds_Status.append([0,0])
+          print(Liste_Seeds2Location)                        
+          print(Liste_Seeds_Status)
           Lecture_Seeds = False
        if Lecture==True:
           Liste_data=Fichier_Ligne.split(" ")
           if Liste_data[0].isdigit():
+             print(Liste_data)
              Destination,Source,Range=Liste_data
              for i in range(len(Liste_Seeds2Location)):
-                if Liste_Seeds_Status[i]==0: 
-                   if (int(Liste_Seeds2Location[i])>=int(Source)) and (int(Liste_Seeds2Location[i])<(int(Source)+int(Range))):   
-                     Liste_Seeds2Location[i]=(int(Destination)-int(Source)+int(Liste_Seeds2Location[i]))
-                     Liste_Seeds_Status[i]=1         
+                #print("____________________________")
+                print(i)
+                Source_Debut = int(Liste_Seeds2Location[i] [0])
+                Source_Fin = int(Liste_Seeds2Location[i] [1])
+                #print(Source_Debut,Source_Fin)
+                #print(Source,Range,Destination)
+                #print(Liste_Seeds2Location)
+                #print(Liste_Seeds_Status)
+                if int(Liste_Seeds_Status[i][0])==0:
+                   if (Source_Debut>=int(Source)) and (Source_Debut)<(int(Source)+int(Range)):   
+                      Liste_Seeds2Location[i][0]=(int(Destination)-int(Source)+int(Liste_Seeds2Location[i][0]))
+                      Liste_Seeds_Status[i][0]=1
+                      #print(Liste_Seeds2Location) 
+                      #print(Liste_Seeds_Status)
+                   if (Source_Debut<int(Source)) and (Source_Fin)>=(int(Source)):   
+                      Liste_Seeds2Location[i][0]=int(Destination)
+                      Liste_Seeds_Status[i][0]=1
+                      Liste_Seeds2Location.append([Source_Debut,(int(Source)-1)])
+                      Liste_Seeds_Status.append([0,0])
+                      #print(Liste_Seeds2Location) 
+                      #print(Liste_Seeds_Status)
+                if int(Liste_Seeds_Status[i][1])==0:
+                   if (Source_Fin>=int(Source)) and (Source_Fin)<(int(Source)+int(Range)):   
+                      Liste_Seeds2Location[i][1]=(int(Destination)-int(Source)+int(Liste_Seeds2Location[i][1]))
+                      Liste_Seeds_Status[i][1]=1
+                      #print(Liste_Seeds2Location) 
+                      #print(Liste_Seeds_Status)
+                   if (Source_Debut<(int(Source)+int(Range))) and ((Source_Fin)>=(int(Source)+int(Range))):   
+                      Liste_Seeds2Location[i][1]=(int(Source)+int(Range)-1)
+                      Liste_Seeds_Status[i][1]=1
+                      Liste_Seeds2Location.append([(2*(int(Source_Debut))-int(Source)),(int(Source_Fin))])
+                      Liste_Seeds_Status.append([0,0])
+                      #print(Liste_Seeds2Location) 
+                      #print(Liste_Seeds_Status)               
           else:
              Lecture=False
-             Liste_Seeds_Status=[0]*(len(Liste_Seeds2Location))             
+             taille = len(Liste_Seeds_Status)
+             Liste_Seeds_Status.clear();
+             for i in range(taille):
+                Liste_Seeds_Status.append([0,0])
+             #print(Liste_Seeds_Status)              
        if Fichier_Ligne.find("-to-")!=(-1):
+          print("_____________________________")
           print(Fichier_Ligne)
-          Lecture=True          
+          Lecture=True
+          #Break = Break +1
+          if Break ==2:
+             print(Liste_Seeds2Location) 
+             print(Liste_Seeds_Status)           
+             Lecture_Boucle = False
     else:
        Lecture=False
        Lecture_Boucle = False
  
 print("_____________________________")
-print("Le minimum est :",min(Liste_Seeds2Location))
+
+minimum=Liste_Seeds2Location[0][0]
+for i in range(len(Liste_Seeds2Location)):
+   if minimum >=(int(Liste_Seeds2Location[i][0])):
+      minimum=int(Liste_Seeds2Location[i][0])
+      print(minimum)
+   
+print("Le minimum est :",minimum)
